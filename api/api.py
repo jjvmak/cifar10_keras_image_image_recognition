00455f6197641c399333cc3b594acb94277e2c8b
Joonas Mäkinen <makinen.jns@gmail.com>
2019-06-30 09:39:34 +0300

    Simple version of working API.

--- api/api.py
+++ api/api.py
@@ -10,59 +10,42 @@ from skimage.transform import resize
 import tensorflow
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras.models import load_model
+
+
+graph = None
+
+
+class Model(object):
+    def __init__(self):
+        print('loading model')
+        self.model = model_from_json(open('model_architecture.json').read())
+        global graph
+        graph = tensorflow.get_default_graph()
+        print('model loaded')
+
+    def __call__(self, img):
+        global graph
+        with graph.as_default():
+            self.model.load_weights('model_weights.h5')
+            pred = self.model.predict(img)
+            print(pred)
+            return pred
 
 
 app = Flask(__name__)
 CORS(app)
-
-# remove this if loading from json works
-model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same', input_shape=(32, 32, 3)))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10))
-model.add(Activation('softmax'))
-
-opt = tensorflow.keras.optimizers.RMSprop(lr=0.0001, decay=1e-6)
-
-model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
-
-model.load_weights('./model_weights.h5')
-
-graph = tensorflow.keras.get_default_graph()
-app.run(host='127.0.0.1', port=5000, threaded=False)
+model = Model()
 
 
 @app.route('/image', methods=['POST'])
 def image():
-    global graph
-    with graph.as_default():
-        if not request.json or not 'image' in request.json:
-            abort(400)
-        img_bytes = BytesIO(base64.b64decode(request.json['image']))
-        img = np.array(Image.open(img_bytes))
-        resized = resize(img, (32, 32), anti_aliasing='reflect', mode='reflect')
-        resized_reshaped = resized.reshape(1, 32, 32, 3)
-        print(resized_reshaped.shape)
-        # TODO: fix this
-        # multithreading problems?
-        pred = model.predict(resized_reshaped)
-        print(pred)
-        return jsonify({'msg': '200 ok'})
+    if not request.json or not 'image' in request.json:
+        abort(400)
+    img_bytes = BytesIO(base64.b64decode(request.json['image']))
+    img = np.array(Image.open(img_bytes))
+    resized = resize(img, (32, 32), anti_aliasing='reflect', mode='reflect')
+    resized_reshaped = resized.reshape(1, 32, 32, 3)
+    print(resized_reshaped.shape)
+    print(model.__call__(resized_reshaped))
+    return jsonify({'msg': '200 ok'})
