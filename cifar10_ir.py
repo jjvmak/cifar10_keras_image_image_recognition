6fdb8907c95f58d51684c8641a2b16a0a4bcd778
Joonas Mäkinen <makinen.jns@gmail.com>
2019-07-23 09:29:45 +0300

    Removed unused functions.

--- cifar10_ir.py
+++ cifar10_ir.py
@@ -1,15 +1,15 @@
 
 import tensorflow
 import pickle
-import numpy as np
-from scipy import stats
-from tensorflow.keras.models import Sequential, model_from_json
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
 from tensorflow.keras.utils import to_categorical
+import os
 
 
 def make_dumps():
+    print('making dumps')
     (x_train, y_train), (x_test, y_test) = \
     tensorflow.keras.datasets.cifar10.load_data()
     pickle.dump(x_test, open("x_test.p", "wb"))
@@ -19,36 +19,14 @@ def make_dumps():
 
 
 def load_dump(dump_name):
+    print('loading dump: ' + dump_name)
     return pickle.load(open(dump_name, "rb"))
 
 
-def first_order_texture_measures(img):
-    r = img[:, :, 0]
-    g = img[:, :, 1]
-    b = img[:, :, 2]
-
-    r_mean = np.mean(r, axis=1)
-    g_mean = np.mean(g, axis=1)
-    b_mean = np.mean(b, axis=1)
-
-    r_var = np.var(r, axis=1)
-    g_var = np.var(g, axis=1)
-    b_var = np.var(b, axis=1)
-
-    fetures = np.concatenate(
-        (r_mean, g_mean, b_mean, r_var, g_var, b_var), axis=0)
-
-    return fetures.reshape(-1, len(fetures))
-
-
-def extract_rgb_features(image_array):
-    features = first_order_texture_measures(image_array[0])
-    for i in range(len(image_array)):
-        if (i != 0):
-            print('viela ' + repr(i))
-            features = np.concatenate((features, first_order_texture_measures(image_array[i])), axis=0)
-    return features
-
+print('looking for dumps')
+if not os.path.isfile('./x_test.p'):
+    print('no such file exists')
+    make_dumps()
 
 x_train = load_dump('x_train.p')
 y_train = load_dump('y_train.p')
@@ -59,7 +37,9 @@ x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
+print('dumps loaded')
 
+print('building model')
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
 model.add(Activation('relu'))
@@ -87,14 +67,14 @@ opt = tensorflow.keras.optimizers.RMSprop(lr=0.0001, decay=1e-6)
 model.compile(loss='categorical_crossentropy',
               optimizer=opt,
               metrics=['accuracy'])
+print('model built')
 
-model.fit(x=x_train, y=y_train_cat, epochs=20)
+print('training model')
+model.fit(x=x_train, y=y_train_cat, epochs=1)
+print('model trained')
 
-filename = 'model_' + '1' + '.h5'
-model.save(filename)
-print('>Saved %s' % filename)
-
-# saving as json
+print('saving model')
 json_string = model.to_json()
 open('./model_architecture.json', 'w').write(json_string)
 model.save_weights('model_weights.h5')
+print('model saved')
